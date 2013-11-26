0d87e23bcdd9e9781a809d0f901845ed478b3cbd
Hugo <hugo.castel@u-psud.fr>
2013-11-26 03:14:16 +0100

    d

--- Test1.c
+++ Test1.c
@@ -13,14 +13,22 @@ struct tmp_image * fils[4] ;
 } r_image ;
 typedef r_image *image ;
 
-// pour compiler dans un terminal
-// gcc Test1.c -o Test1
+/* pour compiler dans un terminal
+ gcc Test1.c -o Test1*/
+ /*
+ git commit -a
+ git pull origin master
+ git push
 
-/*void construit_blanc()
-void construit_noir()*/
+*/
+
+void construit_blanc()
+{
+
+/*
+void construit_noir()
 void construit_composee(i1,i2,i3,i4)
-
-/*void est-noir(r_image A)
+void est-noir(r_image A)
 void est-blanche(r_image A)
 void meme dessin(r_image A, r_image B)
 void simplifie(r_image A)
@@ -33,3 +41,8 @@ void arrondit(r_image A)
 void nebuleuse(int k)
 void Alea(int k, int n)
 void menu(osef)*/
+
+int main()
+{
+}
+{
