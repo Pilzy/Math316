19b94462f941594889f46acffb3e00ea0367440b
Hugo <hugo.castel@u-psud.fr>
2013-11-26 02:27:10 +0100

    Ajout des fichiers

---
+++ Test1.c
@@ -0,0 +1,35 @@
+#define ISNOT !=
+#define NOT !
+#define AND &&
+#define OR ||
+typedef enum {
+false,
+true
+} bool;
+
+typedef struct tmp_image
+{ bool toutnoir ;
+struct tmp_image * fils[4] ;
+} r_image ;
+typedef r_image *image ;
+
+// pour compiler dans un terminal
+// gcc Test1.c -o Test1
+
+/*void construit_blanc()
+void construit_noir()*/
+void construit_composee(i1,i2,i3,i4)
+
+/*void est-noir(r_image A)
+void est-blanche(r_image A)
+void meme dessin(r_image A, r_image B)
+void simplifie(r_image A)
+void copie(r_image A)
+void negatif(r_image A)
+void difference(r_image A, r_image B)
+void aire(r_image A)
+void rendmemoire(r_image A)
+void arrondit(r_image A)
+void nebuleuse(int k)
+void Alea(int k, int n)
+void menu(osef)*/
